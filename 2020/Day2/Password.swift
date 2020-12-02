ec02813da7f00534287ef27d72d66456067b1a83
CWalter50 <walt50@gmail.com>
2020-12-02 14:59:23 -0500

    Create Password.swift

---
+++ 2020/Day2/Password.swift
@@ -0,0 +1,91 @@
+import Foundation
+
+class Password
+{
+  /* Sample:
+  "2-5 w: dgqtwwkwwc" -> for the PW to be valid it needs 2-5 w's in the password. dgqtwwkwwc has 4 w's so it is valid
+
+  */
+  var original: String
+  var pw: String // the password
+  var min: Int // min times letter can appear
+  var max: Int // max times letter can appear
+  var letter: String // this is the letter in the Password
+
+  init(o: String)
+  {
+    self.original = o;
+    self.max = 0
+    self.min = 0
+    self.letter = "a"
+    self.pw = "test"
+    // serach for the dash -.
+    let dashIndex = original.firstIndex(of: "-") ?? original.endIndex
+    self.min = Int(original[..<dashIndex]) ?? 0
+    let spaceIndex = original.firstIndex(of: " ") ?? original.endIndex
+    let afterDashIndex = original.index(dashIndex, offsetBy: 1)
+    self.max = Int(original[afterDashIndex..<spaceIndex]) ?? 0
+    let afterSpaceIndex = original.index(spaceIndex, offsetBy:1)
+    
+    self.letter = String(original[afterSpaceIndex])
+    let colonIndex = original.firstIndex(of: ":") ?? original.endIndex
+    let afterColonIndex = original.index(after: colonIndex)
+    self.pw = String(original[afterColonIndex..<original.endIndex])
+    
+
+    // print("\(original) min: \(min) max: \(max) letter: \(letter) password: \(pw)")
+
+  }
+
+  func isValid() -> Bool
+  {
+    var count = 0
+    for character in self.pw
+    {
+      if String(character) == self.letter
+      {
+        count += 1
+      }
+    }
+
+    if count >= self.min && count <= self.max
+    {
+      return true
+    }
+    else 
+    {
+      return false
+    }
+  }
+
+  func isValid2() -> Bool
+  {
+    // dont have to worry about the -1 in the offset because pw has a space in front of it.
+    let firstIndex = pw.index(pw.startIndex, offsetBy: min)
+    let secondIndex = pw.index(pw.startIndex, offsetBy: max)
+
+    // print(pw[firstIndex])
+    // print(pw[secondIndex])
+    let firstCorrect: Bool = String(pw[firstIndex]) == letter
+    let secondCorrect: Bool = String(pw[secondIndex]) == letter
+    
+    var count = 0
+    if firstCorrect {
+        count += 1
+    }
+    if secondCorrect {
+        count += 1
+    }
+    
+    if count == 1
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+
+  }
+
+}
